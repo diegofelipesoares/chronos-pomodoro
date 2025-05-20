1d991b5dbb4fb3f9c01933311f0fb132da66bcf8
Diego Soares <diegofelipesoares@gmail.com>
2025-05-20 14:36:25 -0300

    Container Heading

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,25 @@ import { Heading } from './components/Heading';
 //importando CSS
 import './styles/theme.css'; //variáveis
 import './styles/global.css'; //global
+import { TimerIcon } from 'lucide-react';
 
 //Escritos em PascalCase. Ex. App, DiegoSoreas, ExDeNome
 
 //cria a função com exportação
 export function App() {
   //Funções JavaScript
-  console.log('Oi');
 
   return (
     // Renderização no HTML - HTML + JS
     // JSX - Só permite um elemento pai no return por isso o fragmento em volta de tudo
     <>
-      <Heading />
+      <Heading>
+        Oi Mundo
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
+
       <div>
         <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,5 +1,11 @@
 import styles from './Heading.module.css';
 
-export function Heading() {
-  return <h1 className={`${styles.cyan} ${styles.heading} `}>Olá Mundo</h1>;
+//Tipando o objeto Children da Props
+type HeadingProps = {
+  children: React.ReactNode;
+};
+
+//Children desestruturado --> Acesso direto sem escrever Props
+export function Heading({ children }: HeadingProps) {
+  return <h1 className={styles.heading}>{children}</h1>;
 }
